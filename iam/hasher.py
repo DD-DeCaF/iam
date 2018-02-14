91cc03b79ec86e822df3f3432600b26ea22c5b58
Ali Kaafarani <ali@kvikshaug.no>
2018-02-14 17:24:49 +0100

    feat: add pbkdf2 sha256 password hasher

---
+++ iam/hasher.py
@@ -0,0 +1,30 @@
+import base64
+import hashlib
+import hmac
+import random
+
+
+def new_salt(n=12):
+    salt_chars = ('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+                  '0123456789')
+    salt = [random.SystemRandom().choice(salt_chars) for _ in range(n)]
+    return ''.join(salt)
+
+
+def encode(password, salt=None, iterations=100000):
+    if salt is None:
+        salt = new_salt()
+    if not isinstance(password, bytes):
+        password = password.encode()
+    hash = hashlib.pbkdf2_hmac(hashlib.sha256().name, password, salt.encode(),
+                               iterations, None)
+    hash = base64.b64encode(hash).decode('ascii').strip()
+    return "{:d}${}${}".format(iterations, salt, hash)
+
+
+def verify(password, encoded):
+    if not isinstance(password, bytes):
+        password = password.encode()
+    iterations, salt, hash = encoded.split('$', 2)
+    encoded_new = encode(password, salt, int(iterations))
+    return hmac.compare_digest(encoded.encode(), encoded_new.encode())
