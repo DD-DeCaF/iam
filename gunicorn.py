4f7f5168a91735fa88c5f6fa88bf84820a26e2ad
Ali Kaafarani <ali@kvikshaug.no>
2018-02-15 09:20:15 +0100

    chore: add access log output

--- gunicorn.py
+++ gunicorn.py
@@ -8,6 +8,8 @@ bind = "0.0.0.0:8000"
 worker_class = "gevent"
 timeout = 30
 loglevel = "info"
+accesslog = "-"
+access_log_format = """%(t)s "%(r)s" %(s)s %(b)s %(L)s "%(f)s\""""
 
 if _configuration == 'prod':
     workers = multiprocessing.cpu_count() * 2 + 1
