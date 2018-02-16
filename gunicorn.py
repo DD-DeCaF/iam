e75643f0211d4798955f2c15e3d612877633037b
Ali Kaafarani <ali@kvikshaug.no>
2018-02-16 10:56:45 +0100

    chore: omit timestamp in gunicorn production log

--- gunicorn.py
+++ gunicorn.py
@@ -9,11 +9,12 @@ worker_class = "gevent"
 timeout = 30
 loglevel = "info"
 accesslog = "-"
-access_log_format = """%(t)s "%(r)s" %(s)s %(b)s %(L)s "%(f)s\""""
 
 if _configuration == 'prod':
     workers = multiprocessing.cpu_count() * 2 + 1
     preload_app = True
+    access_log_format = """"%(r)s" %(s)s %(b)s %(L)s "%(f)s\""""
 else:
     workers = 1
     reload = True
+    access_log_format = """%(t)s "%(r)s" %(s)s %(b)s %(L)s "%(f)s\""""
