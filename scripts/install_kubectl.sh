a6abbc517ff13c86b0ebd754d33e099ded0baa5e
Ali Kaafarani <ali@kvikshaug.no>
2018-05-08 17:13:17 +0200

    chore: add kubernetes ci/cd

--- scripts/install_kubectl.sh
+++ scripts/install_kubectl.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2018, Novo Nordisk Foundation Center for Biosustainability,
-# Technical University of Denmark.
+# Copyright 2018 Novo Nordisk Foundation Center for Biosustainability, DTU.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,9 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eu
+set -xeu
 
-docker tag ${PROD_IMAGE} dddecaf/iam:${TRAVIS_COMMIT::12}
-docker tag ${PROD_IMAGE} dddecaf/iam:${TRAVIS_BRANCH}
-docker push dddecaf/iam:${TRAVIS_COMMIT::12}
-docker push dddecaf/iam:${TRAVIS_BRANCH}
+gcloud --quiet components update kubectl
+gcloud --quiet container clusters get-credentials dd-decaf
