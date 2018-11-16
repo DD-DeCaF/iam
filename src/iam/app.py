fa9944e02f83f0080a7a4688e357c23d63ea65ac
Ali Kaafarani <ali@kvikshaug.no>
2018-11-16 13:24:59 +0100

    docs: update docstring

--- src/iam/app.py
+++ src/iam/app.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Expose the main Flask-RESTPlus application."""
+"""Expose the main Flask application."""
 
 import getpass
 import logging
